ecafb31d7d43e720c90437d375ef4431d22d08d2
edwardchen <499485532@qq.com>
2022-08-29 23:03:52 +0800

    [feature] add backend info log

--- backend/flaskr/predict.py
+++ backend/flaskr/predict.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, redirect
+from flask import Blueprint, request, redirect, current_app
 
 bp = Blueprint('predict', __name__, url_prefix="/predict")
 
@@ -10,6 +10,8 @@ def predict_index():
     if request.method == 'POST':
         selected_models = request.json['selected']
         input_sql = request.json['sql'].upper()
+        current_app.logger.info("request input_sql: '%s'", input_sql)
+        current_app.logger.info("request selected_models: %s", selected_models)
         result = ""
         for model in selected_models:
             result = result + f"[{model}] {input_sql}\n"
