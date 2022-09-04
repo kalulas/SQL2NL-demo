be9ab2e7e4da1468cee75172f39a3dd7bba0c774
edwardchen <499485532@qq.com>
2022-09-04 13:57:42 +0800

    [feature] torch reference test

--- backend/flaskr/sql2text_bridge.py
+++ backend/flaskr/sql2text_bridge.py
@@ -1,7 +1,7 @@
 from flask import current_app
-# from Model.model import RelativeTransformer
-# import torch
+from Model.model import RelativeTransformer
+import torch
 
 def predict(model:str, input_sql:str):
-    current_app.logger.info("you are on bridge")
+    current_app.logger.info(f"you are on bridge, torch version:{torch.__version__}")
     return f"[{model}] {input_sql}\n"
