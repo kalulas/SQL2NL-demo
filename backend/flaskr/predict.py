48de2dbaa5453476a1c9dc64d1dc64e5dfcfbcaf
edwardchen <499485532@qq.com>
2022-09-19 00:17:17 +0800

    after merge from feature-branch

--- backend/flaskr/predict.py
+++ backend/flaskr/predict.py
@@ -1,26 +1,14 @@
 from flask import Blueprint, request, redirect, current_app, jsonify
-from flaskr import sql2text_bridge, utils, file_utils
-from flask_request_id import RequestID
+from flaskr import utils
+from flaskr.evaluation_result import EvaluationResult
 
 bp = Blueprint('predict', __name__, url_prefix="/predict")
 # bp.record_once(sql2text_bridge.setup_checkpoints)
 # bp.record_once(sql2text_bridge.setup_models)
 
-def setup_bridge_if_not_ready():
-    if sql2text_bridge.is_ready():
-        return
-    
-    current_app.logger.info("bridge not ready, setup checkpoints and models...")
-    sql2text_bridge.setup_checkpoints(True)
-    sql2text_bridge.setup_models(True)
-    current_app.logger.info("bridge is now ready!")
-
 
 @bp.route('/', methods=('GET', 'POST'))
 def predict_index():
-    request_ext = RequestID(current_app)
-    current_app.logger.critical("RequestID is %s", request_ext.id)
-    
     identifier = utils.generate_request_id(request.remote_addr)
     if request.method == 'GET':
         return redirect('/../') # to home page
@@ -34,11 +22,14 @@ def predict_index():
         current_app.logger.info("request info: '%s'", request.json)
         # current_app.logger.info("request selected_models: %s", selected_models)
 
-        setup_bridge_if_not_ready()
-
         results = []
         for model in selected_models:
-            result = sql2text_bridge.predict(model, db_id, gold_nl_array, input_sql, identifier)
+            # result = sql2text_bridge.predict(model, db_id, gold_nl_array, input_sql, identifier)
+            result = EvaluationResult()
+            result.modelName = model
+            result.original = input_sql
+            result.failedReason = 'pytorch not available on this branch'
+
             results.append(result.toDict())
             # result = result + prediction
 
@@ -52,6 +43,6 @@ def process_models_request():
 
 @bp.route('/databases/', methods=['POST'])
 def process_databases_request():
-    available_database_names = file_utils.get_databases_from_file(sql2text_bridge.TRAIN_TABLE_FILE_PATH)
+    available_database_names = ['not available']
     # current_app.logger.info(available_database_names)
     return jsonify(available_database_names)
